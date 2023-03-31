b18891e50ea908799f97514d0b2151b91833046f
Karissa <ka.lange266@gmail.com>
2023-03-30 20:36:51 -0400

    add hourly weather, basic design

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let locationName = document.getElementById('locationName')
-let getLocationButton = document.querySelector('button')
+let getLocationButton = document.getElementById('searchButton')
 let currentTemperature = document.getElementById('currentTemperature')
 let uvIndex = document.getElementById('uvIndex')
 let rainChance = document.getElementById('rainChance')
@@ -18,9 +18,24 @@ function getWeather(){
           console.log(data)
           currentTemperature.innerText = Math.floor(data.current.temp_f)
           currentWeatherIcon.src = data.current.condition.icon
-          locationName.innerText = data.location.name
+          let cityName = data.location.name
+          let stateName = data.location.region
+          locationName.innerText = `${cityName}, ${stateName}`
           
-          
+          //hourlyWeather icons
+          document.getElementById('hour0600img').src = data.forecast.forecastday[0].hour[6].condition.icon
+          document.getElementById('hour0900img').src = data.forecast.forecastday[0].hour[9].condition.icon
+          document.getElementById('hour1200img').src = data.forecast.forecastday[0].hour[12].condition.icon
+          document.getElementById('hour1500img').src = data.forecast.forecastday[0].hour[15].condition.icon
+          document.getElementById('hour1800img').src = data.forecast.forecastday[0].hour[18].condition.icon
+
+          //hourlyWeather Conditions
+          document.getElementById('hour0600condition').innerText = Math.floor(data.forecast.forecastday[0].hour[6].temp_f)
+          document.getElementById('hour0900condition').innerText = Math.floor(data.forecast.forecastday[0].hour[9].temp_f)
+          document.getElementById('hour1200condition').innerText = Math.floor(data.forecast.forecastday[0].hour[12].temp_f)
+          document.getElementById('hour1500condition').innerText = Math.floor(data.forecast.forecastday[0].hour[3].temp_f)
+          document.getElementById('hour1800condition').innerText = Math.floor(data.forecast.forecastday[0].hour[6].temp_f)
+
         })
         .catch(err => {
             console.log(`error ${err}`)
