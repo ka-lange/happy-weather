5458c86c40b64d4110dc48d54413766605967212
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-08-02 10:18:41 -0400

    remove server routes and models

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const indexController = require('../controllers/index')
-
-router.get('/', indexController.getIndex) //uses home controller to get index page and render index.js from views
-
-module.exports = router
